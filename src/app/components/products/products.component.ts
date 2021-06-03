18d8b580dd686017bba9f13b845f0f8c59ebd5e4
Amine Reda <amine.reda037@gmail.com>
2021-06-03 16:05:03 +0100

    add angular project

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from 'src/app/model/product.model';
+import { ProductsService } from 'src/app/services/products.service';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { AppDataState, DataStateEnum } from 'src/app/state/product.state';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent implements OnInit {
+  products$: Observable<AppDataState<Product[]>> | null = null;
+  readonly DataStateEnum = DataStateEnum;
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  onGetAllProducts() {
+    this.products$ = this.productsService.getAllProducts().pipe(
+      map((data) => {
+        console.log(data);
+        return {
+          dataState: DataStateEnum.LOADED,
+          data: data,
+        };
+      }),
+      startWith({ dataState: DataStateEnum.LOADING }),
+      catchError((err) =>
+        of({ dataState: DataStateEnum.ERROR, errorMessage: err.message })
+      )
+    );
+  }
+
+  onGetSelectedProducts() {
+    this.products$ = this.productsService.getSelectedProducts().pipe(
+      map((data) => {
+        console.log(data);
+        return {
+          dataState: DataStateEnum.LOADED,
+          data: data,
+        };
+      }),
+      startWith({ dataState: DataStateEnum.LOADING }),
+      catchError((err) =>
+        of({ dataState: DataStateEnum.ERROR, errorMessage: err.message })
+      )
+    );
+  }
+  onGetAvailableProducts() {
+    this.products$ = this.productsService.getAvailableProducts().pipe(
+      map((data) => {
+        console.log(data);
+        return {
+          dataState: DataStateEnum.LOADED,
+          data: data,
+        };
+      }),
+      startWith({ dataState: DataStateEnum.LOADING }),
+      catchError((err) =>
+        of({ dataState: DataStateEnum.ERROR, errorMessage: err.message })
+      )
+    );
+  }
+
+  onSearch(dataForm: any) {
+    this.products$ = this.productsService.SearchProducts(dataForm.keyword).pipe(
+      map((data) => {
+        console.log(data);
+        return {
+          dataState: DataStateEnum.LOADED,
+          data: data,
+        };
+      }),
+      startWith({ dataState: DataStateEnum.LOADING }),
+      catchError((err) =>
+        of({ dataState: DataStateEnum.ERROR, errorMessage: err.message })
+      )
+    );
+  }
+  onSelect(p: Product) {
+    this.productsService.select(p).subscribe((data) => {
+      p.selected = data.selected;
+    });
+  }
+
+  onDelete(p: Product) {
+    let v = confirm('Etes vous sure ?');
+    if (v == true) {
+      this.productsService.delete(p).subscribe((data) => {
+        this.onGetAllProducts();
+      });
+    }
+  }
+  onNewProduct() {
+    this.router.navigateByUrl('/newProduct');
+  }
+
+  onEdite(p: Product) {
+    this.router.navigateByUrl('/editProduct/' + p.id);
+  }
+}
